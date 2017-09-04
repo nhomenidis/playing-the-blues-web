729a26eda3c6afbc9b2a7ef61852830fc98a52a4
Nikos Chomenidis <nhomenidis@me.com>
2017-09-04 22:04:56 +0300

    Bootstrap playing-the-blues-web Project

---
+++ Controllers/BluesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlayingTheBlues.Models;
+
+namespace PlayingTheBlues.Controllers
+{
+    [Route("api/blues")]
+    public class BluesController : Controller
+    {
+        [HttpGet]
+        public BluesChords GetChords([FromQuery] string tonic)
+        {
+            var chords = new BluesChords();
+            chords.Tonic = tonic;
+
+            return chords;
+        }
+    }
+}
---
+++ Models/BluesChords.cs
@@ -0,0 +1,9 @@
+namespace PlayingTheBlues.Models
+{
+    public class BluesChords
+    {
+        public string Tonic { get; set; }
+        public string SubDominant { get; set; }
+        public string Dominant { get; set; }
+    }
+}
