4e696e7e0e12c5180af4b7d3e6b079130a69872a
Nikos Chomenidis <nhomenidis@me.com>
2017-09-06 10:25:02 -0700

    The algorithm that finds the chords to be used

--- Controllers/BluesController.cs
+++ Controllers/BluesController.cs
@@ -16,7 +16,36 @@ namespace PlayingTheBlues.Controllers
             var chords = new BluesChords();
             chords.Tonic = tonic;
 
-            return chords;
+            List<string> tones = new List<string>();//the list from the tones you can choose
+            tones.Add("Cb");
+            tones.Add("Gb");
+            tones.Add("Db");
+            tones.Add("Ab");
+            tones.Add("Eb");
+            tones.Add("Bb");
+            tones.Add("F");
+            tones.Add("C");
+            tones.Add("G");
+            tones.Add("D");
+            tones.Add("A");
+            tones.Add("E");
+            tones.Add("B");
+            tones.Add("F#");
+            tones.Add("C#");
+
+            bool check = tones.Contains(tonic); //check if the tone exists in tones list
+            if (check)
+            {
+                int rootindex, subdomindex, domindex;//variables for the position of I,IV,V chords
+                rootindex = tones.BinarySearch(tonic);//user input as I chord
+                subdomindex = rootindex - 1;// index of IV chord (thing the cycle of 4ths, counterclockwise)
+                domindex = rootindex + 1;// index of V chord (thing the cycle of 5ths, clockwise)
+
+                chords.SubDominant = tones[subdomindex];
+                chords.Dominant = tones[domindex];
+            }
+
+                return chords;
         }
     }
 }
